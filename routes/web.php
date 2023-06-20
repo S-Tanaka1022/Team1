a85497bbfd490b450d5f3c67483e2b290b09cd21
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-20 10:17:00 +0900

    fukuyama__test_change

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/fukuyama', function(){
+    print("fukuyama");
+});
 require __DIR__ . '/auth.php';
