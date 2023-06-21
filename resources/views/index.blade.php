9753d20d007b9ebc51b6de434e6d8c9033235a5e
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-21 15:26:07 +0900

    フェイル作成：indexファイルをログインしたら表示する。ログインしている人を表示、ログアウトもできる。

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,29 @@
-<h1>index.blade.php</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+</head>
 
-<?php
+<body>
+    <header>
+        <h1>index.blade.php</h1>
+        <p>
+            {{ Auth::user() -> name }} さんログイン中
+        </p>
+    </header>
 
+    <main>
+        <form action="{{ route('logout') }}" method="post" class="max-w-md mx-auto px-4 py-6 bg-white rounded-md shadow-md">
+            <button type="submit" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer">ログアウト</button>
+            @csrf
+        </form>
+    </main>
 
+    <footer>
 
+    </footer>
+</body>
+</html>
