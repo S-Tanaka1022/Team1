c0cb961d6b197beaa2bbac17ebe5dafa29f651b5
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-21 17:52:06 +0900

    【APIテスト】曲名を設定することで曲名、アーティスト名、ジャケ写が表示できる

--- resources/views/myplaylist.blade.php
+++ resources/views/myplaylist.blade.php
@@ -1,2 +1,37 @@
 <h1>myplaylist.blade.php</h1>
 <?php
+
+use SpotifyWebAPI\SpotifyWebAPI;
+use SpotifyWebAPI\Session;
+
+// Spotify APIクライアントの初期化
+$session = new Session(
+    'f172da853aeb4266863fb2661addbb76',
+    'bcf72a943e1245828831cda721f77987'
+);
+$session->requestCredentialsToken();
+$accessToken = $session->getAccessToken();
+
+$api = new SpotifyWebAPI();
+$api->setAccessToken($accessToken);
+
+// 曲の検索
+$results = $api->search('ex. ダーリン', 'track');
+
+// 検索結果から曲の情報を取得
+$songs = $results->tracks->items;
+?>
+
+@foreach ($songs as $song)
+    <?php
+        $trackName = $song->name;
+        $artistName = $song->artists[0]->name;
+        $albumImage = $song->album->images[0]->url;
+    ?>
+
+    <div>
+        <p>{{ $trackName }}</p>
+        <p>{{ $artistName }}</p>
+        <img src="{{ $albumImage }}" alt="Album Image">
+    </div>
+@endforeach
