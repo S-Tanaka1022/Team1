2411d3a138ce7c002c5cd18b13fea19134a85093
Ichimura <h-ichimura@usen-next.jp>
2023-06-21 17:25:21 +0900

    みんなのプレイリスト画面テスト１

--- resources/views/everyone_playlist.blade.php
+++ resources/views/everyone_playlist.blade.php
@@ -4,45 +4,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>みんなのプレイリスト</title>
 </head>
+
 <body>
     <h1>みんなのプレイリスト</h1>
-
-    <div>
-        <form action="" method="GET">
-            <label>
-                検索キーワード
-                <input type="text" name="keyword" value="{{ $keyword }}">
-            </label>
-            <input type="submit" value="検索">
-        </form>
-    </div>
-
-    <div>
-        <form action="" method="GET">
-            <button type="button" name="add_list" value="add_list">プレイリスト作成</button>
-        </form>
-    </div>
-
-    <table border='1'>
-        <tr>
-            <th>user</th>
-            <th>playlist</th>
-            <th>詳細</th>
-        </tr>
-        @foreach ($playlists as $playlist)
-            <tr>
-                <td>{{$playlist->name}}</td>
-                <td>{{$playlist->playlist}}</td>
-                <td><a href="/other_playlist">詳細</a></td>
-            </tr>
-        @endforeach
-    </table>
-
+    <form action="/index" method="GET">
+        <button type="button" name="weather" value="weather">天気情報</button>
+    </form>
+    <form action="/myplaylists" method="GET">
+        <button type="button" name="mylist" value="mylist">マイプレイリスト</button>
+    </form>
     <form action="{{route('logout')}}" method="post">
         <button type="submit">ログアウト</button>
         @csrf
     </form>
+
+    <p id="tabcontrol">
+        <a href="#tabpage1">楽曲一覧</a>
+        <a href="#tabpage2">プレイリスト一覧</a>
+     </p>
+
+     <div id="tabbody">
+        <div id="tabpage1">
+            <div>
+                <form action="" method="GET">
+                    <label>
+                        検索キーワード
+                        {{-- <input type="text" name="keyword" value="{{$keyword}}"> --}}
+                    </label>
+                    <input type="submit" value="検索">
+                </form>
+            </div>
+
+            <table border='1'>
+                <tr>
+                    <th>No</th>
+                    <th>music</th>
+                    <th>artist</th>
+                </tr>
+                {{--
+                @foreach ($playlists as $playlist)
+                    <tr>
+                        <td>{{$playlist->name}}</td>
+                        <td>{{$playlist->playlist}}</td>
+                        <td><a href="/other_playlist">詳細</a></td>
+                    </tr>
+                @endforeach
+                --}}
+            </table>
+        </div>
+
+
+
+        <div id="tabpage2">
+            <div>
+                <form action="" method="GET">
+                    <label>
+                        検索キーワード
+                        {{-- <input type="text" name="keyword" value="{{$keyword}}"> --}}
+                    </label>
+                    <input type="submit" value="検索">
+                </form>
+            </div>
+
+            <table border='1'>
+                <tr>
+                    <th>user</th>
+                    <th>playlist</th>
+                    <th>詳細</th>
+                </tr>
+                {{--
+                @foreach ($playlists as $playlist)
+                    <tr>
+                        <td>{{$playlist->name}}</td>
+                        <td>{{$playlist->playlist}}</td>
+                        <td><a href="/other_playlist">詳細</a></td>
+                    </tr>
+                @endforeach
+                --}}
+            </table>
+        </div>
+     </div>
+
+    <script src="{{ asset('/js/everyone_playlist.js') }}"></script>
 </body>
 </html>
