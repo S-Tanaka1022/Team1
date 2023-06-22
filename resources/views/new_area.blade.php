c74fd5462bb910eb8c69b5ed3e5f3e56a58eb829
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-22 12:05:38 +0900

    ファイル編集：POSTで送られてきたものを表示可能。

--- resources/views/new_area.blade.php
+++ resources/views/new_area.blade.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>初回現在地登録</title>
+    <title>エリア選択画面</title>
 </head>
 <body>
     <header>
-        <h1>初回現在地登録</h1>
+        <h1>エリア選択画面</h1>
     </header>
 
     <main>
-        都道府県選択
-        <form action="new_area" method="POST">
-            <select name="sel_region">
-                @foreach($regions as $region)
-                    <option value="{{$region -> region_code}}">{{$region -> region_name}}</option>
+        エリア選択
+        @dump($areas_data)
+        <form action="" method="POST">
+            <select name="sel_area">
+                @foreach($areas_data as $areas)
+                    <option value="{{$areas -> area -> code}}">{{$areas -> area -> name}}</option>
                 @endforeach
             </select>
             <input type="submit" value="選択">
@@ -25,4 +26,3 @@
     </main>
 </body>
 </html>
-
