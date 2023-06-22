02281425489d349273528cd41ca561159a9d15d5
ShuTanaka <s-tanaka-a@usen-next.jp>
2023-06-23 07:52:10 +0900

    ファイル編集：index.blade.phpで登録地追加ボタンを作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,5 @@
 @php
 use App\Models\Region_name;
-
-#表示するのは、都道府県名（DB）・地域名(API)・気象情報(API)
-
 @endphp
 
 
@@ -26,11 +23,14 @@ use App\Models\Region_name;
     <main>
         <div>
 
-        </div>
         <form action="{{ route('logout') }}" method="post" class="max-w-md mx-auto px-4 py-6 bg-white rounded-md shadow-md">
             <button type="submit" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer">ログアウト</button>
             @csrf
         </form>
+        <form action="add_region" method="get">
+            <button type="submit">登録地追加</button>
+        </form>
+        </div>
 
         <div>
             <p>
@@ -44,6 +44,7 @@ use App\Models\Region_name;
                     </tr>
 
                     @php
+                    #表示するのは、都道府県名（DB）・地域名(API)・気象情報(API)
                             foreach ($fav_regions as $fav_region){
                                 $region_code = $fav_region["region_code"];
                                 $area_code = $fav_region["area_code"];
--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::post('/code_save', [RegionController::class, 'update']);
 Route::get('/add_region', [RegionController::class, 'add']);
 Route::post('/add_area', [RegionController::class, 'add_area']);
 
-#削除昨日
+#削除機能
 Route::get('/delete/{id}', [RegionController::class, 'delete']);
 
 #自分のプレイリスト一覧画面
