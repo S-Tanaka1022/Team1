2854b2010435c812de2edad6234c0482b9783e14
ShuTanaka <s-tanaka-a@usen-next.jp>
2023-06-22 16:58:16 +0900

    ファイル編集：テーブル形式に挿入と、そのためのコントローラーの編集

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Region;
+use App\Models\Region_name;
+
 
 class UserController extends Controller
 {
-    public function index(){
-        {
-            return view('index');
+    public function index()
+    { {
+            $user_id = auth()->user()->id;
+            $regions = Region_name::all();
+            $fav_regions = Region::where('user_id', "$user_id")->get();
+
+            return view('index', compact('regions', 'fav_regions'));
         }
     }
 }
