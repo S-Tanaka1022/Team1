8719e80b5998f4c87a8c6d9381d64348c1d2d560
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-23 10:34:53 +0900

    #60 【チェックポイント】getTrackで曲の詳細情報はとれた（アーティスト情報を取得中）

--- resources/views/song_information.blade.php
+++ resources/views/song_information.blade.php
@@ -1,8 +1,40 @@
 <?php
-// dump($track);
-$trackName = $track->name;
-$artistName = $track->artists[0]->name; // 一番最初のアーティスト名を使用する例
+dump($track);
+//曲の詳細情報
+$trackName = $track->name; //曲名
+$artistName = $track->artists[0]->name; //アーティスト名
+$trackTime = $track->duration_ms; //曲の再生時間
+    $seconds = floor($trackTime/1000);
+    $minutes = floor($seconds/60);
+    $seconds = $seconds%60;
+    $secondsFormat = sprintf('%02d', $seconds);
+$albumName = $track->album->name; //アルバム名
+$trackPreview = $track->preview_url; //プレビューのURL(日本の曲は対応してないことが多い)
 
-echo "Track Name: $trackName" . PHP_EOL;
-echo "Artist Name: $artistName" . PHP_EOL;
+//アーティスト情報
+$artists = $track->artists;
+// $artistImage = $artists->images[0]->url;
+// $genres = [];
+// foreach($artists as $artist){
+//     $genres = array_merge($genres, $artist->genres);
+// }
+// $genres = array_unique($genres);
+
+echo "Song Detail<br>";
+echo "Track Name: $trackName<br>" . PHP_EOL;
+echo "Album Name: $albumName<br>" . PHP_EOL;
+echo "Artist Name: $artistName<br>" . PHP_EOL;
+echo "Duration Time: $minutes:$secondsFormat<br>" . PHP_EOL;
+if ($trackPreview) {
+    echo '<audio controls>';
+    echo "<source src=\"$trackPreview\" type=\"audio/mpeg\">";
+    echo 'Your browser does not support the audio element.';
+    echo '</audio>';
+    echo "<br>";
+} else {
+    echo 'Preview not available.<br>';
+}
+
+echo "Artist Information<br>";
+
 ?>
