555253e080661fed420311520c5d8c7689f88756
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-27 18:46:58 +0900

    【CSS】#80 曲追加画面をきれいにした

--- resources/views/add_myplaylist.blade.php
+++ resources/views/add_myplaylist.blade.php
@@ -1,3 +1,21 @@
+@php
+$trackImage = $track->album->images[0]->url; //アルバム画像
+$releaseDate = $track->album->release_date; //リリース日
+    $dateFormat = date("Y年m月d日", strtotime($releaseDate));
+$trackName = $track->name; //曲名
+$artistName = $track->artists[0]->name; //アーティスト名
+$trackTime = $track->duration_ms; //曲の再生時間
+    $seconds = floor($trackTime/1000);
+    $minutes = floor($seconds/60);
+    $seconds = $seconds%60;
+    $secondsFormat = sprintf('%02d', $seconds);
+$albumName = $track->album->name; //アルバム名
+$trackPreview = $track->preview_url; //プレビューのURL(日本の曲は対応してないことが多い)
+
+//アーティスト情報
+$artistImage = $artist->images[0]->url; //アーティストの宣材写真
+@endphp
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -41,32 +59,70 @@
             </ul>
         </nav>
     </header>
-    <form action="" method="POST">
-        プレイリスト名
-        <input type="text" name="playlist_name" placeholder="新規プレイリスト">
+    <div class="container m-10 p-10 rounded bg-dark text-white">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-auto text-center">
+                    <div class="text-center"><br>
+                        <img src={{$trackImage}} width=350><br>
+                        <span style="font-size: 40px;" class="fw-bold">{{$trackName}}</span>　　{{$minutes}}:{{$secondsFormat}}<br>
+                        <span style="font-size: 20px;" class="fw-bold">{{$albumName}}</span><br>
+                        <div class="container-fluid">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-auto text-center">
+                                    <img src={{$artistImage}} width=100 class="rounded-circle">
+                                </div>
+                                <div class="fs-1 text-left align-middle">{{$artistName}}<br>
+                                    @foreach($artist->genres as $genre)
+                                        {{$genre}}　
+                                    @endforeach<br>
+                                    {{$dateFormat}}
+                                </div>
+                            </div>
+                        </div><br>
+                        @if ($trackPreview)
+                            <audio controls class="w-400">
+                                <source src="{{$trackPreview}}" type="audio/mpeg">
+                                Your browser does not support the audio element.
+                            </audio><br>
+                        @else
+                            Preview not available.<br>
+                        @endif
+                    </div><br>
+                </div>
+                <div class="col-md-auto">
+                    <div class="text-right m-1"> <!-- 右寄せの要素 -->
+                        <form action="" method="POST">
+                            <span class="m-2">プレイリスト名 </span>
+                            <input type="text" name="playlist_name" placeholder="新規プレイリスト"><br><br>
+                            <span class="m-2">既存プレイリスト </span>
+                            <select name="list_id">
+                                <option hidden>プレイリスト選択</option>
+                                @foreach ($playlists as $playlist)
+                                    <option value="{{$playlist->id}}">{{$playlist->list_name}}</option>
+                                @endforeach
+                            </select>
+                            <br><br>
 
-        <select name="list_id">
-            <option hidden>プレイリスト選択</option>
-            @foreach ($playlists as $playlist)
-                <option value="{{$playlist->id}}">{{$playlist->list_name}}</option>
-            @endforeach
-        </select>
-        <br>
+                            曲名
+                            <input type="text" name="title" value="{{$track->name}}" readonly><br><br>
+                            アーティスト
+                            <input type="text" name="artist" value="{{$track->artists[0]->name}}" readonly><br><br>
 
-        曲名
-        <input type="text" name="title" value="{{$track->name}}" readonly><br>
-        アーティスト
-        <input type="text" name="artist" value="{{$track->artists[0]->name}}" readonly><br>
-
-        {{-- trackIdの流用 --}}
-        <input type="hidden" name="trackId" value="{{$trackId}}">
-        <input type="submit" value="追加">
-        @csrf
-    </form>
-
-    <form action="{{route('logout')}}" method="post">
-        <button type="submit">ログアウト</button>
-        @csrf
-    </form>
+                            {{-- trackIdの流用 --}}
+                            <input type="hidden" name="trackId" value="{{$trackId}}">
+                            <input type="submit" value="追加" class="btn btn-success btn-block"><br>
+                            @csrf
+                        </form>
+                    </div>
+                    <div class="text-right"> <!-- 右寄せの要素 -->
+                        <button class="btn btn-info btn-block" name="back" onclick="location.href='/everyone_playlist'">
+                            楽曲一覧に戻る
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/css/index_css.css',
                 'resources/css/region_area.css',
+                'resources/css/stylesheet.css'
             ],
             refresh: true,
         }),
