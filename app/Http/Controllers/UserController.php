ef6274dd2979adaa1cab0b5d5f7c0cc44984e74f
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-28 16:34:08 +0900

    indexの微調整

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,6 @@ use App\Models\Region_name;
 use SpotifyWebAPI\SpotifyWebAPI;
 use SpotifyWebAPI\Session;
 
-
 class UserController extends Controller
 {
     public function index()
@@ -17,15 +16,7 @@ class UserController extends Controller
         $regions = Region_name::all();
         $fav_regions = Region::where('user_id', "$user_id")->get();
         if ($fav_regions->count() > 0) {
-            $session = new Session(
-                'f172da853aeb4266863fb2661addbb76',
-                'bcf72a943e1245828831cda721f77987'
-            );
-            $session->requestCredentialsToken();
-            $accessToken = $session->getAccessToken();
-
-            $api = new SpotifyWebAPI();
-            $api->setAccessToken($accessToken);
+            $api = Controller::getAPI();
             return view('index', compact('regions', 'fav_regions', 'api'));
         } else {
             return redirect('/new_region');
