e8708563be85f47f34ea18c75daaa8ca10e14721
takano kento <kn-takano@usen-next.jp>
2023-06-28 15:40:45 +0900

    髙野更新

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,6 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/index_css.css',
                 'resources/css/region_area.css',
-<<<<<<< HEAD
-=======
-                'resources/css/stylesheet.css'
->>>>>>> origin/Develop
             ],
             refresh: true,
         }),
