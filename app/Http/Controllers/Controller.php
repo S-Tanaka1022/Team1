f581254686b01ee507c966995d62abdab51c3008
ShuTanaka <s-tanaka-a@usen-next.jp>
2023-06-29 17:32:00 +0900

    ファイル編集：APIの取得のリファクタリング

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,10 +7,12 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use SpotifyWebAPI\Session;
 use SpotifyWebAPI\SpotifyWebAPI;
+use App\Models\Region_name;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
 
     public static function getAPI()
     {
@@ -24,5 +26,35 @@ class Controller extends BaseController
         $api->setAccessToken($accessToken);
 
         return $api;
+    }
+
+    public static function get_weatherAPI($fav_regions)
+    {
+        $API_data = []; // Initialize as an empty array
+
+        foreach ($fav_regions as $fav_region) {
+            $region_code = $fav_region["region_code"];
+            $area_code = $fav_region["area_code"];
+            $id = $fav_region["id"];
+
+            $region = Region_name::where('region_code', "$region_code")->get();
+            $region_data = json_decode($region, true);
+            $prefecture = $region_data[0]["region_name"];
+
+            $url = "https://www.jma.go.jp/bosai/forecast/data/forecast/{$region_code}.json";
+            $response = file_get_contents($url);
+            $data = json_decode($response, true);
+            $areas_data = $data[0]["timeSeries"][0]["areas"];
+            $area = $areas_data[$area_code]["area"]["name"];
+            $weathers = $areas_data[$area_code]["weathers"];
+
+            $API_data[] = [ // Append a new array to $API_data
+                'id' => $id,
+                'prefecture' => $prefecture,
+                'area' => $area,
+                'weathers' => $weathers,
+            ];
+        }
+        return $API_data;
     }
 }
