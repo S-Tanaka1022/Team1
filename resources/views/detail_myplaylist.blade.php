0ca4e0dd988d4dd1eef42b26a82b01c201a4dc01
takano kento <kn-takano@usen-next.jp>
2023-06-29 17:24:40 +0900

    検索枠中央寄せ

--- resources/views/detail_myplaylist.blade.php
+++ resources/views/detail_myplaylist.blade.php
@@ -23,7 +23,7 @@
                 </li>
                 <li class="nav-item">
                     <form action="everyone_playlist" method="get">
-                        <button class="btn btn-primary mr-3" type="submit">みんなのプレイリスト</button>
+                        <button class="btn btn-primary mr-3" type="submit">楽曲一覧</button>
                     </form>
                 </li>
                 <li class="nav-item">
@@ -40,54 +40,59 @@
             </ul>
         </nav>
     </header>
-    <main class="m-3">
-        <button class="btn btn-info btn-block btn-lg" onclick="goBack()">
-            <b>戻る</b>
-        </button>
-
-        <script>
-        function goBack() {
-        window.history.back();
-        }
-        </script>
-        <form action="" method="get" >
-            <label>
-                <input type="hidden" name = "playlist_id" value="{{$playlistId}}">
-                <input type="text" name="keyword3" placeholder="検索キーワード">
-            </label>
-            <input type="submit" class="btn btn-primary" value="検索">
-        </form>
-
+    <div class="container align-middle text-center">
+        <div class="row">
+            <div class="col-2">
+                <main class="m-3 text-left">
+                    <button class="btn btn-info" onclick="goBack()">
+                        <b>戻る</b>
+                    </button>
+                    <script>
+                        function goBack() {
+                            window.history.back();
+                        }
+                    </script>
+                </div>
+                <div class="col-5 text-right m-3">
+                    <form action="" method="get" >
+                        <label>
+                            <input type="hidden" name = "playlist_id" value="{{$playlistId}}">
+                            <input type="text" name="keyword3" placeholder="検索キーワード">
+                        </label>
+                        <input type="submit" class="btn btn-primary" value="検索">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
         @if(count($tracks) == 0)
             <p>検索結果は見つかりませんでした</p>
         @else
-        <table class="table">
-            <tr class="bg-dark text-white text-center align-middle">
+        <table class="table table-striped text-center align-middle">
+            <tr class="bg-dark text-white">
                 <th>ジャケット写真</th>
                 <th>曲名</th>
                 <th>アーティスト名</th>
                 <th>詳細情報</th>
                 <th>曲を削除</th>
-
             </tr>
             @foreach ($tracks as $track)
-            <tr class="text-center align-middle">
-                <td class="text-center align-middle"><img src="{{$track["detail"]->album->images[0]->url}}" width=80></td>
-                <td class="text-center align-middle"><b>{{$track["detail"]->name}}</b></td>
+            <tr>
+                <td class="text-center align-middle col-2"><img src="{{$track["detail"]->album->images[0]->url}}" width=80></td>
+                <td class="text-center align-middle col-3"><b>{{$track["detail"]->name}}</b></td>
                 <td class="text-center align-middle"><b>{{$track["detail"]->artists[0]->name}}</b></td>
                 <form action="information" method="get" enctype="multipart/form-data">
-                    <td class="text-center align-middle"><button class="btn btn-info" type="submit" name="information" value='{{$track["detail"]->id}}'>詳細情報</button></td>
+                    <td class="text-center align-middle"><button class="btn align-middle btn-info" type="submit" name="information" value='{{$track["detail"]->id}}'>詳細情報</button></td>
                 </form>
                 <form action="/delete_myplaylist_song" method="get">
-                <input type="hidden" name="playlistId" value="{{$playlist->id}}">
-                <td class="text-center align-middle"><button class="btn btn-danger" type="submit" name="song_detail_id" value='{{$track["song_primary_key"]}}'>削除</button></td>
+                    <input type="hidden" name="playlistId" value="{{$playlist->id}}">
+                    <td class="text-center align-middle col-2"><button class="btn text-center align-middle btn-danger" type="submit" name="song_detail_id" value='{{$track["song_primary_key"]}}'>削除</button></td>
                 </form>
             </tr>
             @csrf
             @endforeach
         </table>
         @endif
-    </main>
 </body>
 </html>
