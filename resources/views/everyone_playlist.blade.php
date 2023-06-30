71babc4c1ae8f6105cfa36f68944e41883afef25
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-30 11:33:29 +0900

    微調整：更新ボタンのアイコンを微調整

--- resources/views/everyone_playlist.blade.php
+++ resources/views/everyone_playlist.blade.php
@@ -62,28 +62,25 @@
           <a class="Tabs__presentation-slider" role="presentation"></a>
         </nav>
 
-        <div class="tabs__body" style="text-align: center">
-          <div class="tabs__content active" data-tab-content="">
-            <div class="container align-middle text-center">
-                <div class="row align-items-center text-center">
-
-                        <button class="btn btn-primary d-flex align-items-center material-symbols-outlined" name="back" onclick="location.href=''">
-                            楽曲更新
-                            <span class="material-symbols-outlined2">
-                                refresh
-                            </span>
-                            </button>
-
-                    <div class="col-8">
-                        <form action="" method="GET">
-                            <label>
-                                <input type="text" name="keyword" placeholder="検索キーワード">
-                            </label>
-                            <input type="submit" class="btn btn-primary" value="検索">
-                        </form>
-                    </div>
-                </div>
+        <div class="container align-middle text-center">
+            <div class="row">
+              <button class="btn btn-primary d-flex justify-content-center align-items-center material-symbols-outlined" name="back" onclick="location.href=''">
+                楽曲更新
+                <span class="material-symbols-outlined2 align-middle test">
+                  refresh
+                </span>
+              </button>
+              <div class="col-8">
+                <form action="" method="GET">
+                  <label>
+                    <input type="text" name="keyword" placeholder="検索キーワード">
+                  </label>
+                  <input type="submit" class="btn btn-primary" value="検索">
+                </form>
+              </div>
             </div>
+          </div>
+
 
             <table class="table table-striped text-center align-middle mt-4">
                 <tr class="bg-dark text-white">
