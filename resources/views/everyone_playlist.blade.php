5ace5570fef9687769fc84503be865f3d8f54785
Ichimura <h-ichimura@usen-next.jp>
2023-06-30 11:19:22 +0900

    仮）更新ボタン

--- resources/views/everyone_playlist.blade.php
+++ resources/views/everyone_playlist.blade.php
@@ -62,9 +62,9 @@
             <div class="container align-middle text-center">
                 <div class="row align-items-center text-center">
 
-                        <button class="btn btn-primary material-symbols-outlined" name="back" onclick="location.href=''">
+                        <button class="btn btn-primary d-flex align-items-center material-symbols-outlined" name="back" onclick="location.href=''">
                             楽曲更新
-                            <span>
+                            <span class="material-symbols-outlined2">
                                 refresh
                             </span>
                             </button>
